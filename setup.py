c2db9468b13f846420a84537dc1aa85f0c5ca49c
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2017-12-12 17:48:26 +0100

    fix unit issue in cjam/_axi.pyx and duplicate files ready for cylindrical dispersion code

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ from Cython.Build import cythonize
 
 sources = ["cjam/_jam_axi.pyx"]
 interp = ["src/interp/interp2dpol.c"]
-jam = ["src/jam/jam_axi_rms.c", "src/jam/jam_axi_rms_mgeint.c",
+jam = ["src/jam/jam_axi_cylin_rms.c", "src/jam/jam_axi_cylin_rms_mgeint.c",
+    "src/jam/jam_axi_cylin_rms_mmt.c", "src/jam/jam_axi_cylin_rms_wmmt.c"
+    "src/jam/jam_axi_rms.c", "src/jam/jam_axi_rms_mgeint.c",
     "src/jam/jam_axi_rms_mmt.c", "src/jam/jam_axi_rms_wmmt.c",
     "src/jam/jam_axi_vel.c", "src/jam/jam_axi_vel_losint.c",
     "src/jam/jam_axi_vel_mgeint.c", "src/jam/jam_axi_vel_mmt.c",
