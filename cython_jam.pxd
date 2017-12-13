ff236d43441af270a21a05b13d43bb45a8f6e457
Prashin Jethwa <pjethwa@ma023782.ads.eso.org>
2017-12-13 17:55:14 +0100

    fix typos

--- cython_jam.pxd
+++ cython_jam.pxd
@@ -17,5 +17,5 @@ cdef extern from "../src/jam/jam.h":
     void jam_axi_cylin_rms(double *xp, double *yp, int nxy, double incl, \
         double *lum_area, double *lum_sigma, double *lum_q, int lum_total, \
         double *pot_area, double *pot_sigma, double *pot_q, int pot_total, \
-        double *beta, int nrad, int nang, double *rxx, double *ryy, double *rzz, \
-        );
+        double *beta, int nrad, int nang, double *rxx, double *ryy, \
+        double *rzz);
--- src/jam/jam.h
+++ src/jam/jam.h
@@ -80,8 +80,7 @@ double* jam_axi_rms_wmmt( double *, double *, int, double, \
 void jam_axi_cylin_rms(double *xp, double *yp, int nxy, double incl, \
     double *lum_area, double *lum_sigma, double *lum_q, int lum_total, \
     double *pot_area, double *pot_sigma, double *pot_q, int pot_total, \
-    double *beta, int nrad, int nang, double *rxx, double *ryy, double *rzz, \
-    double *rxy, double *rxz, double *ryz);
+    double *beta, int nrad, int nang, double *rxx, double *ryy, double *rzz);
 
 double jam_axi_cylin_rms_mgeint( double, void * );
 
